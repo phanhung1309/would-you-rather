6641149f45338a0401b30acaaa51c0c0e0ed1e26
Hung Phan <phanhung1309@gmail.com>
2022-07-10 09:47:38 +0700

    Not found page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Button, Header, Image } from "semantic-ui-react";
 import "./styles.css";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate("/");
+  };
+
   return (
-    <div className="container">
-      <h1>NotFound page</h1>
+    <div className="notFoundContainer">
+      <Image src="/assets/koala.png" size="medium" />
+      <Header as="h1">Oops! Page Not Found</Header>
+      <p>The page you're looking for does not seem to exist</p>
+      <Button content="Go back Home..." color="green" onClick={handleGoBack} />
     </div>
   );
 };
