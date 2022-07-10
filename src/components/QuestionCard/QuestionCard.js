c74dcab2d19e984aeea16395a9e729f5ea4bc0e9
Hung Phan <phanhung1309@gmail.com>
2022-07-10 09:19:17 +0700

    Home screen UI

---
+++ src/components/QuestionCard/QuestionCard.js
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  SegmentGroup,
+  Header,
+  Grid,
+  GridColumn,
+  Image,
+  Button,
+} from "semantic-ui-react";
+
+const QuestionCard = ({ questionData, author, isAnswered }) => {
+  const buttonContent = isAnswered ? "View Result" : "View Poll";
+
+  const buttonColor = isAnswered ? "blue" : "green";
+
+  const handleClick = () => {};
+
+  return (
+    <SegmentGroup>
+      <Header as="h5" block>
+        {author.name} asks:
+      </Header>
+
+      <Grid divided padded>
+        <GridColumn width={4}>
+          <Image src={author.avatarURL} />
+        </GridColumn>
+        <GridColumn width={12}>
+          <Header as="h5">Would You Rather?</Header>
+          <p>... {questionData.optionOne.text} ...</p>
+          <Button
+            fluid
+            color={buttonColor}
+            content={buttonContent}
+            onClick={handleClick}
+          />
+        </GridColumn>
+      </Grid>
+    </SegmentGroup>
+  );
+};
+
+QuestionCard.propTypes = {
+  questionData: PropTypes.object.isRequired,
+  author: PropTypes.object.isRequired,
+  isAnswered: PropTypes.bool.isRequired,
+};
+
+export default QuestionCard;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,77 @@
 import React from "react";
+import { Tab } from "semantic-ui-react";
 import "./styles.css";
+import { useSelector } from "react-redux";
+import { mapValues } from "lodash";
+import QuestionCard from "../../components/QuestionCard";
 
 const Home = () => {
+  const { userId } = useSelector((state) => state.auth);
+  const { users } = useSelector((state) => state.users);
+  const { questions } = useSelector((state) => state.questions);
+
+  const answeredQuestions = () => {
+    const _answeredQuestions = [];
+
+    mapValues(questions, (question) => {
+      if (Object.keys(users[userId].answers).includes(question.id)) {
+        _answeredQuestions.push(question);
+      }
+    });
+
+    return _answeredQuestions;
+  };
+
+  const unansweredQuestions = () => {
+    const _unansweredQuestions = [];
+
+    mapValues(questions, (question) => {
+      if (!Object.keys(users[userId].answers).includes(question.id)) {
+        _unansweredQuestions.push(question);
+      }
+    });
+
+    return _unansweredQuestions;
+  };
+
+  const panes = [
+    {
+      menuItem: "Unanswered",
+      render: () => (
+        <Tab.Pane>
+          {unansweredQuestions().map((question) => (
+            <QuestionCard
+              key={question.id}
+              questionData={question}
+              author={users[question.author]}
+              isAnswered={false}
+            />
+          ))}
+        </Tab.Pane>
+      ),
+    },
+    {
+      menuItem: "Answered",
+      render: () => (
+        <Tab.Pane>
+          {answeredQuestions().map((question) => (
+            <QuestionCard
+              key={question.id}
+              questionData={question}
+              author={users[question.author]}
+              isAnswered={true}
+            />
+          ))}
+        </Tab.Pane>
+      ),
+    },
+  ];
+
   return (
     <div className="container">
-      <h1>Home page</h1>
+      <div className="homeContainer">
+        <Tab panes={panes} menu={{ secondary: true, pointing: true }} />
+      </div>
     </div>
   );
 };
